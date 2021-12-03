a56323e5885d5772ceb1058555a7a641328ea841
Keurcien Luu <keurcien@MacBook-Pro-de-Keurcien.local>
2021-12-03 16:28:51 +0100

    Transform acquisition data

--- models/example/ads.sql
+++ models/example/ads.sql
@@ -10,13 +10,24 @@
 {{ config(materialized='table') }}
 
 select
-    "_airbyte_data" ->> 'OS' as os,
-    ("_airbyte_data" ->> 'ATC')::integer as add_to_cart
+    ("_airbyte_data" ->> 'Semaine')::text as week,
+    ("_airbyte_data" ->> 'Premier Jour Semaine')::text as week_first_day,
+    ("_airbyte_data" ->> 'Période')::integer as "period",
+    ("_airbyte_data" ->> 'Media source')::text as media_source,
+    ("_airbyte_data" ->> 'Ad set')::text as adset,
+    ("_airbyte_data" ->> 'campagne')::integer as campaign,
+    ("_airbyte_data" ->> 'Type de campagne \n(Retargeting, Cold acquisition, A/B tests..)')::text as campaign_type,
+    ("_airbyte_data" ->> 'Objectif de la campagne \n(App install, product sales..)')::text as campaign_objective,
+    ("_airbyte_data" ->> 'OS')::text as os,
+    ("_airbyte_data" ->> 'Impression Device')::text as impression_device,
+    ("_airbyte_data" ->> 'NEW USER vs OLD USER')::text as user_type,
+    ("_airbyte_data" ->> 'impressions')::integer as impressions,
+    ("_airbyte_data" ->> 'Clics')::integer as clicks,
+    ("_airbyte_data" ->> 'App installs')::integer as app_installs,
+    ("_airbyte_data" ->> 'ATC')::integer as add_to_cart,
+    ("_airbyte_data" ->> 'Uniq ATC')::text as unique_add_to_cart,
+    ("_airbyte_data" ->> 'Estimated Uniq ATC')::text as estimated_add_to_cart,
+    ("_airbyte_data" ->> 'Estimated Uniq ATC')::text as estimated_unique_add_to_cart,
+    ("_airbyte_data" ->> 'Estimated Uniq Purchase')::text as estimated_unique_purchases,
+    ("_airbyte_data" ->> 'Cost')::numeric as cost
 from airbyte."_airbyte_raw_data___all_canaux__a_exporter_"
-limit 10
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
