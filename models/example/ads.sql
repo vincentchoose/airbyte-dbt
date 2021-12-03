cfa9692ff690239052766fb6dad8e309aa88873a
Keurcien Luu <keurcien@MacBook-Pro-de-Keurcien.local>
2021-12-03 16:33:47 +0100

    Transform acquisition data

--- models/example/ads.sql
+++ models/example/ads.sql
@@ -12,10 +12,10 @@
 select
     ("_airbyte_data" ->> 'Semaine')::text as week,
     ("_airbyte_data" ->> 'Premier Jour Semaine')::text as week_first_day,
-    ("_airbyte_data" ->> 'Période')::integer as "period",
+    ("_airbyte_data" ->> 'Période')::text as "period",
     ("_airbyte_data" ->> 'Media source')::text as media_source,
     ("_airbyte_data" ->> 'Ad set')::text as adset,
-    ("_airbyte_data" ->> 'campagne')::integer as campaign,
+    ("_airbyte_data" ->> 'campagne')::text as campaign,
     ("_airbyte_data" ->> 'Type de campagne \n(Retargeting, Cold acquisition, A/B tests..)')::text as campaign_type,
     ("_airbyte_data" ->> 'Objectif de la campagne \n(App install, product sales..)')::text as campaign_objective,
     ("_airbyte_data" ->> 'OS')::text as os,
@@ -25,9 +25,9 @@ select
     ("_airbyte_data" ->> 'Clics')::integer as clicks,
     ("_airbyte_data" ->> 'App installs')::integer as app_installs,
     ("_airbyte_data" ->> 'ATC')::integer as add_to_cart,
-    ("_airbyte_data" ->> 'Uniq ATC')::text as unique_add_to_cart,
-    ("_airbyte_data" ->> 'Estimated Uniq ATC')::text as estimated_add_to_cart,
-    ("_airbyte_data" ->> 'Estimated Uniq ATC')::text as estimated_unique_add_to_cart,
-    ("_airbyte_data" ->> 'Estimated Uniq Purchase')::text as estimated_unique_purchases,
+    ("_airbyte_data" ->> 'Uniq ATC')::integer as unique_add_to_cart,
+    ("_airbyte_data" ->> 'Estimated Uniq ATC')::integer as estimated_add_to_cart,
+    ("_airbyte_data" ->> 'Estimated Uniq ATC')::integer as estimated_unique_add_to_cart,
+    ("_airbyte_data" ->> 'Estimated Uniq Purchase')::integer as estimated_unique_purchases,
     ("_airbyte_data" ->> 'Cost')::numeric as cost
 from airbyte."_airbyte_raw_data___all_canaux__a_exporter_"
